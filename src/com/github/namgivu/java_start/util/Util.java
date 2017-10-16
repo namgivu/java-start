04be49feea14e3377f52218059e52c2385af4d9c
namgivu <namgivu@gmail.com>
2017-10-16 17:35:58 +0700

    redirect via util method

--- src/com/github/namgivu/java_start/util/Util.java
+++ src/com/github/namgivu/java_start/util/Util.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Util {
 
 	public static void redirectIO(String CODE_HOME, String VAULT_FOLDER, String CHALLENGE_NAME) throws FileNotFoundException {
-		//region io - redirect I/O to files
+		//redirect I/O to files
 		String CHALLENGE_HOME	= String.format("%s/%s/%s", CODE_HOME, VAULT_FOLDER, CHALLENGE_NAME);
 		String IO_FILE_INPUT	= String.format("%s/io/input.txt",  CHALLENGE_HOME);
 		String IO_FILE_OUTPUT	= String.format("%s/io/output.txt", CHALLENGE_HOME);
