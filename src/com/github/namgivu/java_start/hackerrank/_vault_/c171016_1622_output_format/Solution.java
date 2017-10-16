4813a7ba007e5f186f67d235c0ca4c7e2df86c7f
namgivu <namgivu@gmail.com>
2017-10-16 17:10:11 +0700

    more challenge

---
+++ src/com/github/namgivu/java_start/hackerrank/_vault_/c171016_1622_output_format/Solution.java
@@ -0,0 +1,33 @@
+package com.github.namgivu.java_start.hackerrank._vault_.c171016_1622_output_format;
+
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Solution {
+
+	//challenge ref. https://www.hackerrank.com/challenges/java-output-formatting/problem
+	static String CODE_HOME = "/home/namgvu/NN/code/java-start/src/com/github/namgivu/java_start";
+	static String VAULT_FOLDER = "hackerrank/_vault_";
+	static String CHALLENGE_NAME = "c171016_1622_output_format";
+
+	public static void main(String[] args) throws FileNotFoundException {
+		//region io - redirect I/O to files
+		String CHALLENGE_HOME = String.format("%s/%s/%s", CODE_HOME, VAULT_FOLDER, CHALLENGE_NAME);
+		String IO_FILE_INPUT = String.format("%s/io/input.txt", CHALLENGE_HOME);
+		String IO_FILE_OUTPUT = String.format("%s/io/output.txt", CHALLENGE_HOME);
+		System.setIn(new FileInputStream(IO_FILE_INPUT)); //ref. https://stackoverflow.com/a/188654/248616
+		System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(IO_FILE_OUTPUT)), true)); //ref. https://stackoverflow.com/a/2851254/248616
+		//endregion io - redirect I/O to files
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("================================");
+		for (int line = 0; line < 3; line++) {
+			String s = scan.next();
+			int i = scan.nextInt();
+			System.out.println(String.format("%-15s%03d", s, i));
+		}
+		System.out.println("================================");
+	}
+
+}
