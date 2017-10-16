c33445c663e35bbc8d171bd1f4e82b8239205cf2
namgivu <namgivu@gmail.com>
2017-10-16 13:28:46 +0700

    in out 0th

---
+++ src/com/github/namgivu/java_start/hackerrank/c171016_1100_inout/Solution.java
@@ -0,0 +1,19 @@
+package com.github.namgivu.java_start.hackerrank.c171016_1100_inout;
+
+import java.util.Scanner;
+
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
