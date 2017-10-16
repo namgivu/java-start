abd87ef9ff9573c6fbe4cf2be9de15f4b70811ee
namgivu <namgivu@gmail.com>
2017-10-16 17:12:52 +0700

    spacing

--- src/com/github/namgivu/java_start/hackerrank/cYYmmdd_hhMM_TEMPLATE/Solution.java
+++ src/com/github/namgivu/java_start/hackerrank/cYYmmdd_hhMM_TEMPLATE/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
 	public static void main(String[] args) throws FileNotFoundException {
 		//region io - redirect I/O to files
 		String CHALLENGE_HOME	= String.format("%s/%s/%s", CODE_HOME, VAULT_FOLDER, CHALLENGE_NAME);
-		String IO_FILE_INPUT		= String.format("%s/io/input.txt",  CHALLENGE_HOME);
+		String IO_FILE_INPUT	= String.format("%s/io/input.txt",  CHALLENGE_HOME);
 		String IO_FILE_OUTPUT	= String.format("%s/io/output.txt", CHALLENGE_HOME);
 		System.setIn(new FileInputStream(IO_FILE_INPUT)); //ref. https://stackoverflow.com/a/188654/248616
 		System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(IO_FILE_OUTPUT)), true)); //ref. https://stackoverflow.com/a/2851254/248616
