8c2d1ae2f410a08f7debb6cd3fdece18cf633024
namgivu <namgivu@gmail.com>
2017-10-16 13:33:23 +0700

    use string format

--- src/com/github/namgivu/java_start/hackerrank/cYYmmdd_hhMM_TEMPLATE/Solution.java
+++ src/com/github/namgivu/java_start/hackerrank/cYYmmdd_hhMM_TEMPLATE/Solution.java
@@ -8,8 +8,9 @@ public class Solution {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		//region io - redirect to files
-		String IO_FILE_INPUT	= "/home/namgvu/NN/code/java-start/src/com/github/namgivu/java_start/hackerrank/cYYmmdd_hhMM_TEMPLATE/io/input.txt";
-		String IO_FILE_OUTPUT	= "/home/namgvu/NN/code/java-start/src/com/github/namgivu/java_start/hackerrank/cYYmmdd_hhMM_TEMPLATE/io/output.txt";
+		String CODE_HOME = "/home/namgvu/NN/code/java-start/src/com/github/namgivu/java_start";
+		String IO_FILE_INPUT	= String.format("%s/hackerrank/cYYmmdd_hhMM_TEMPLATE/io/input.txt", CODE_HOME);
+		String IO_FILE_OUTPUT	= String.format("%s/hackerrank/cYYmmdd_hhMM_TEMPLATE/io/output.txt", CODE_HOME);
 		System.setIn(new FileInputStream(IO_FILE_INPUT)); //ref. https://stackoverflow.com/a/188654/248616
 		System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(IO_FILE_OUTPUT)), true)); //ref. https://stackoverflow.com/a/2851254/248616
 		//endregion io - redirect to files
