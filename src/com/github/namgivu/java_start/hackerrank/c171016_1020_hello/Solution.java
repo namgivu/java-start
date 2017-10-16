af3b2df47b042c9caecfb07c6a7e1649eb245a7b
namgivu <namgivu@gmail.com>
2017-10-16 13:28:31 +0700

    challenge 0th

---
+++ src/com/github/namgivu/java_start/hackerrank/c171016_1020_hello/Solution.java
@@ -0,0 +1,10 @@
+package com.github.namgivu.java_start.hackerrank.c171016_1020_hello;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+
+}
