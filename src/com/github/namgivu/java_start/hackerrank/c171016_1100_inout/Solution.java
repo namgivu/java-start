2cdfddc89b3cb031684edcb5a936c9203678c949
namgivu <namgivu@gmail.com>
2017-10-16 16:22:11 +0700

    challenge review - use template with redirected i/o

--- src/com/github/namgivu/java_start/hackerrank/c171016_1100_inout/Solution.java
+++ src/com/github/namgivu/java_start/hackerrank/c171016_1100_inout/Solution.java
@@ -1,11 +1,22 @@
 package com.github.namgivu.java_start.hackerrank.c171016_1100_inout;
 
+import java.io.*;
 import java.util.Scanner;
 
 
 public class Solution {
+	static String CHALLENGE_NAME	= "hackerrank/c171016_1100_inout";
+	static String CODE_HOME 			= "/home/namgvu/NN/code/java-start/src/com/github/namgivu/java_start";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		//region io - redirect I/O to files
+		String CHALLENGE_HOME	= String.format("%s/%s", CODE_HOME, CHALLENGE_NAME);
+		String IO_FILE_INPUT		= String.format("%s/io/input.txt",  CHALLENGE_HOME);
+		String IO_FILE_OUTPUT	= String.format("%s/io/output.txt", CHALLENGE_HOME);
+		System.setIn(new FileInputStream(IO_FILE_INPUT)); //ref. https://stackoverflow.com/a/188654/248616
+		System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(IO_FILE_OUTPUT)), true)); //ref. https://stackoverflow.com/a/2851254/248616
+		//endregion io - redirect I/O to files
+
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
