7e7e9302353ddd5318d5dcb14540e91479fc5997
namgivu <namgivu@gmail.com>
2017-10-17 14:41:41 +0700

    more challenge

---
+++ src/com/github/namgivu/java_start/hackerrank/_vault_/c171017/c171017_1311_syntax_check/Solution.java
@@ -0,0 +1,43 @@
+package com.github.namgivu.java_start.hackerrank._vault_.c171017.c171017_1311_syntax_check;
+
+import com.github.namgivu.java_start.util.Util;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+public class Solution {
+
+	//challenge ref. https://www.hackerrank.com/challenges/pattern-syntax-checker/problem
+	static String CODE_HOME = "/home/namgvu/NN/code/java-start/src/com/github/namgivu/java_start";
+	static String VAULT_FOLDER = "hackerrank/_vault_/c171017";
+	static String CHALLENGE_NAME = "c171017_1311_syntax_check";
+
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Util.redirectIO(CODE_HOME, VAULT_FOLDER, CHALLENGE_NAME);
+
+		Scanner scan = new Scanner(System.in);
+		int testCases = Integer.parseInt(scan.nextLine());
+		int i=0;
+		while (i<testCases) {
+			String pattern = scan.nextLine();
+			i+=1;
+
+			boolean ok;
+			try {
+				Pattern.compile(pattern);
+				ok = true;
+			} catch (Exception e) {
+				ok = false;
+			}
+
+			System.out.println(ok?"Valid":"Invalid");
+		}
+
+	}
+
+}
