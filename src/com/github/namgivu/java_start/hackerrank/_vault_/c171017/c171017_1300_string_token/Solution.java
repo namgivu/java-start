dcc63a5e3b3f0d19aa3cc349d9608c1e25cd5f90
namgivu <namgivu@gmail.com>
2017-10-17 14:35:05 +0700

    code review

--- src/com/github/namgivu/java_start/hackerrank/_vault_/c171017/c171017_1300_string_token/Solution.java
+++ src/com/github/namgivu/java_start/hackerrank/_vault_/c171017/c171017_1300_string_token/Solution.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Solution {
 
-	//challenge ref. https://www.hackerrank.com/challenges/java-string-reverse
+	//challenge ref. https://www.hackerrank.com/challenges/java-string-tokens/problem
 	static String CODE_HOME 			= "/home/namgvu/NN/code/java-start/src/com/github/namgivu/java_start";
 	static String VAULT_FOLDER		= "hackerrank/_vault_/c171017";
 	static String CHALLENGE_NAME	= "c171017_1300_string_token";
