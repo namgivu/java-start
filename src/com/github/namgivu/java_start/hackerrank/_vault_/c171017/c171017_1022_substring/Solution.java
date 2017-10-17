3feb68f8e4f41d394ac12ab0c935758724b87516
namgivu <namgivu@gmail.com>
2017-10-17 10:53:10 +0700

    rename

--- src/com/github/namgivu/java_start/hackerrank/_vault_/c171017/c171017_1022_substring/Solution.java
+++ src/com/github/namgivu/java_start/hackerrank/_vault_/c171017/c171017_1022_substring/Solution.java
@@ -1,9 +1,8 @@
-package com.github.namgivu.java_start.hackerrank._vault_.c171017.c171017_102_substring;
+package com.github.namgivu.java_start.hackerrank._vault_.c171017.c171017_1022_substring;
 
 import com.github.namgivu.java_start.util.Util;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -12,7 +11,7 @@ public class Solution {
 	//challenge ref. https://www.hackerrank.com/challenges/java-substring/problem
 	static String CODE_HOME 			= "/home/namgvu/NN/code/java-start/src/com/github/namgivu/java_start";
 	static String VAULT_FOLDER		= "hackerrank/_vault_/c171017";
-	static String CHALLENGE_NAME	= "c171017_102_substring";
+	static String CHALLENGE_NAME	= "c171017_1022_substring";
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Util.redirectIO(CODE_HOME, VAULT_FOLDER, CHALLENGE_NAME);
